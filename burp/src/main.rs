5451967f4966edde6155ad347fc5565b40c0f000
Jakob Sanowski <jakob.sanowski@sanbach.de>
2025-04-01 19:52:24 +0200

    perf: switch to 'MessagePack' + use memory mapped files
    Former-commit-id: 855c2a08415a487feddd4b1f84cd6ad907c6d421

--- burp/src/main.rs
+++ burp/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    fs::File,
+    fs::{File, OpenOptions},
     io::{BufReader, BufWriter, Read, Write},
     path::{Path, PathBuf},
     sync::{Arc, RwLock},
@@ -24,7 +24,10 @@ use graph_rs::{
     Graph,
 };
 use indicatif::ProgressBar;
-use rand::prelude::*;
+use memmap2::MmapOptions;
+use rand::{prelude::*, rng, seq::index::sample};
+use rmp_serde::{Deserializer, Serializer};
+use serde::{Deserialize, Serialize};
 
 #[derive(Parser)]
 struct Cli {
@@ -45,6 +48,10 @@ enum Commands {
         /// Points of intrest.
         #[arg(short, long)]
         pois: Option<PathBuf>,
+
+        /// Sample <NUMBER> pois form nodes at random.
+        #[arg(short, long, conflicts_with = "pois")]
+        sample: Option<usize>,
     },
     Build {
         /// Input graph in '.gfb' format
@@ -57,10 +64,6 @@ enum Commands {
         /// Set output file to <FILE>. Defaults to '<IN_FILE>.ocl'.
         #[arg(short = 'o', long)]
         out_file: Option<PathBuf>,
-
-        /// Enable parallel computation (experimental)
-        #[arg(short, long, default_value_t = false)]
-        parallel: bool,
     },
 }
 
@@ -73,48 +76,64 @@ fn main() {
             in_file,
             out_file,
             pois,
+            sample: sample_size,
         } => {
             let out_file = out_file.unwrap_or_else(|| {
                 let mut out_file = in_file.clone();
-                out_file.set_extension("gfb");
+                out_file.set_extension("gmp");
                 out_file
             });
             let in_file = File::open(in_file).unwrap();
-            let reader = BufReader::new(in_file);
+            let in_file_mmap = unsafe { MmapOptions::new().map(&in_file).unwrap() };
+
             let mut graph_writer = GraphWriter::default();
 
-            read_geojson(reader, &mut graph_writer).unwrap();
+            read_geojson(in_file_mmap.as_ref(), &mut graph_writer).unwrap();
 
             let mut graph = PoiGraph::new(RTreeGraph::new_from_graph(graph_writer.get_graph()));
 
             if let Some(pois) = pois {
                 let mut poi_writer = PoiWriter::new(|_| true);
                 read_geojson(BufReader::new(File::open(pois).unwrap()), &mut poi_writer).unwrap();
-                graph.add_pois(poi_writer.pois()).unwrap();
+                graph.add_coord_pois(poi_writer.pois()).unwrap();
             }
 
-            let mut writer = BufWriter::new(File::create(out_file).unwrap());
-            writer.write_all(graph.to_flexbuffer().as_slice()).unwrap();
+            if let Some(sample_size) = sample_size {
+                let pois = sample(&mut rng(), graph.graph().node_count(), sample_size)
+                    .into_iter()
+                    .map(|node_id| (node_id, vec![]))
+                    .collect();
+                graph.add_node_pois(pois)
+            }
+
+            let out_file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open(out_file)
+                .unwrap();
+
+            let mut writer = BufWriter::new(out_file);
+            let mut rmp_serializer = Serializer::new(writer);
+            graph.serialize(&mut rmp_serializer);
         }
         Commands::Build {
             in_file,
             out_file,
             epsilon,
-            parallel,
         } => {
             let out_file = out_file.unwrap_or_else(|| {
                 let mut out_file = in_file.clone();
-                out_file.set_extension("ocl");
+                out_file.set_extension("omp");
                 out_file
             });
 
-            let mut f_buf = vec![];
-            File::open(in_file)
-                .unwrap()
-                .read_to_end(&mut f_buf)
-                .unwrap();
+            let in_file = File::open(in_file).unwrap();
+            let in_file_mmap = unsafe { MmapOptions::new().map(&in_file).unwrap() };
 
-            let mut graph: PoiGraph<Poi> = PoiGraph::read_flexbuffer(f_buf.as_slice());
+            let mut rmp_deserializer = Deserializer::new(in_file_mmap.as_ref());
+
+            let mut graph: PoiGraph<Poi> = PoiGraph::deserialize(&mut rmp_deserializer).unwrap();
 
             let mut oracle = oracle::Oracle::new();
 
@@ -126,7 +145,8 @@ fn main() {
             );
 
             let mut writer = BufWriter::new(File::create(out_file).unwrap());
-            writer.write_all(oracle.to_flexbuffer().as_slice()).unwrap();
+            let mut rmp_serializer = Serializer::new(writer);
+            oracle.serialize(&mut rmp_serializer);
         }
     }
 }
